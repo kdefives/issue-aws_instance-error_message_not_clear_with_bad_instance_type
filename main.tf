6c58e4e2249a42cda1aa7025ae20a1bfc7b12e14
Kevin Defives <kevin.defives@decathlon.com>
2019-06-05 13:34:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+#Terraform v0.12.1
+#+ provider.aws v2.7.0
+
 provider "aws" {
     access_key = ""
     secret_key = ""
@@ -7,7 +10,7 @@ provider "aws" {
 resource "aws_instance" "instance_1"{
     ami = "ami-XXXX" #Replace here by your own AMI
     instance_type = "m5.4xlarge" #This instance_type exists on Europe region but not in China (eg. cn-north-1)
-    tags{
+    tags = {
         Name = "Hello World"
     }
     vpc_security_group_ids = ["sg-XXXX"] #Replace here by your securityGroupID
