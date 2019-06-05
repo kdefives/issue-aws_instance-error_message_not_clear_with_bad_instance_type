ddef23534f3fec4fcf3bdcb0f6ac346ad98a0e57
Kevin Defives <kevin.defives@decathlon.com>
2019-06-05 13:21:43 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    access_key = ""
+    secret_key = ""
+    region     = "cn-north-1"
+}
+
+resource "aws_instance" "instance_1"{
+    ami = "ami-XXXX" #Replace here by your own AMI
+    instance_type = "m5.4xlarge" #This instance_type exists on Europe region but not in China (eg. cn-north-1)
+    tags{
+        Name = "Hello World"
+    }
+    vpc_security_group_ids = ["sg-XXXX"] #Replace here by your securityGroupID
+    subnet_id = "subnet-XXXX" #Replace here by your subnetID
+}
